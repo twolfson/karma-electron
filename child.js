152c65f446e832589918e5190839f4d9f34a25c1
Todd Wolfson <todd@twolfson.com>
2019-06-18 03:11:44 -0700

    Added more content that could be affecting Electron

--- child.js
+++ child.js
@@ -3,12 +3,22 @@ process.on('uncaughtException', function handleUncaughtException (err) {
 });
 
 var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
 
 app.on('window-all-closed', function handleWindowsClosed () {
   app.quit();
 });
 
+app.setPath('userData', __dirname + '/tmp');
+
+      // fs.readFile(program.extendedOptions, 'utf8', function handleReadFile (err, extendedOptionsStr) {
+
+
 app.on('ready', function handleReady () {
+  var browserWindow = new BrowserWindow();
+  browserWindow.loadURL('https://google.com/');
+
   console.log('Loaded');
-  process.nextTick(process.exit);
+
+  setTimeout(process.exit, 3000);
 });
