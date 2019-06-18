97f998f75f53d1dbad883c7af6174005fc0801a5
Todd Wolfson <todd@twolfson.com>
2019-06-18 04:01:35 -0700

    Ditched child/parent files

--- parent.js
+++
@@ -1,9 +0,0 @@
-// Turned out we were fucked from the start =/
-//   https://github.com/electron/electron/issues/4218
-// Need to move to a temporary file
-// Can pass it in as a parameter to load
-var electronPrebuilt = require('electron');
-var execSync = require('child_process').execSync;
-var path = require('path');
-
-execSync(electronPrebuilt + ' ' + path.join(__dirname, 'lib', 'child.js'), {stdio: ['pipe', 'inherit', 'inherit']});
