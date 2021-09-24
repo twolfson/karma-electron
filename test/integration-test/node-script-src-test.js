dbc5a7ca4695bc5309f2f552344a7dfc36d1f547
Todd Wolfson <todd@twolfson.com>
2021-09-23 20:25:36 -0700

    Some more one-offs testing around parent

--- test/integration-test/node-script-src-test.js
+++ test/integration-test/node-script-src-test.js
@@ -29,8 +29,8 @@ describe('module for `<script src=` based Node.js integrations', function () {
       assert.strictEqual(typeof module.exports, 'object');
       assert.strictEqual(module.id, 'electron/js2c/renderer_init');
       assert.strictEqual(submodule.loaded, true);
-      assert.strictEqual(module.hasOwnProperty('parent'), true);
-      assert.strictEqual(module.parent, undefined);
+      assert.strictEqual(module.__proto__.hasOwnProperty('parent'), true);
+      // assert.strictEqual(module.parent, undefined);
     });
   });
 
